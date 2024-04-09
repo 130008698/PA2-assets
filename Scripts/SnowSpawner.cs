116227801ad103ddb9d32884510c2426107aa596
jacoby0729 <jacoby0729@gmail.com>
2024-04-08 20:07:40 -0500

    Level 1-2

---
+++ Scripts/SnowSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowSpawner : MonoBehaviour
+{
+    public GameObject snow;
+    public float spawnInterval = 0.5f;
+
+    void Start()
+    {
+        InvokeRepeating("spawnSnow", 0f, spawnInterval);
+    }
+
+    void spawnSnow()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-5f, 5f), -2f + transform.position.y, transform.position.z);
+        Instantiate(balloonPrefab, spawnPosition, Quaternion.identity);
+    }
+}
